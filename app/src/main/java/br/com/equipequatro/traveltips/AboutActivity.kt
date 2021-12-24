a8ca8ee73f3e638898b877c9b148f2f09f92902e
Leonardo Marcião Ott da Costa <l.ott.da.costa@avanade.com>
2021-12-23 21:41:49 -0300

    Inclusão do menu lateral, botão de new post e Activity de About

---
+++ app/src/main/java/br/com/equipequatro/traveltips/AboutActivity.kt
@@ -0,0 +1,11 @@
+package br.com.equipequatro.traveltips
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AboutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about)
+    }
+}
--- app/src/main/java/br/com/equipequatro/traveltips/MainActivity.kt
+++ app/src/main/java/br/com/equipequatro/traveltips/MainActivity.kt
@@ -1,17 +1,17 @@
 package br.com.equipequatro.traveltips
 
 import android.os.Bundle
-import android.widget.ScrollView
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import br.com.equipequatro.traveltips.databinding.ActivityMainBinding
 import br.com.equipequatro.traveltips.ui.favorites.FavoritesFragment
 import br.com.equipequatro.traveltips.ui.feeds.FeedsFragment
 import br.com.equipequatro.traveltips.ui.home.HomeFragment
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,6 +22,21 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
+        val drawerLayout: DrawerLayout = binding.drawerLayout
+        val navViewDrawer: NavigationView = binding.navViewDrawer
+        val toolbar: Toolbar = binding.toolbar
+
+        setSupportActionBar( toolbar!! )
+
+        var toogle = ActionBarDrawerToggle(this, drawerLayout, toolbar, 0, 0)//
+        drawerLayout.addDrawerListener(toogle)
+        toogle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(false)
+        supportActionBar?.setHomeButtonEnabled(true)
+
 
         val navView: BottomNavigationView = binding.navView
 //        val scrollViewFragment: ScrollView = binding.scrollViewFragment
@@ -53,5 +68,13 @@ class MainActivity : AppCompatActivity() {
             .beginTransaction()
             .replace(R.id.frameLayoutFragment, HomeFragment())
             .commit()
+    }
+
+    override fun onBackPressed() {
+        if (binding.drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            binding.drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
