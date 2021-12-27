bb8732f88b1b1a2875d3db0533a8a188430b2f97
prigagliardill <priscilasgagliardi@gmail.com>
2021-12-27 15:25:57 -0300

    correção tela sobre

--- app/src/main/java/br/com/equipequatro/traveltips/view/fragment/ProfileFragment.kt
+++ app/src/main/java/br/com/equipequatro/traveltips/view/fragment/ProfileFragment.kt
@@ -13,6 +13,8 @@ private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
 class ProfileFragment : Fragment() {
+
+
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -51,5 +53,6 @@ class ProfileFragment : Fragment() {
                     putString(ARG_PARAM2, param2)
                 }
             }
-    }
+        }
 }
+
